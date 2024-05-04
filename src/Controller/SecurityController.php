99930ebdd157769355b7c3c43972329bd9b09a1d
tboyer <thomas.boyer2@interieur.gouv.fr>
2024-05-04 21:42:16 +0200

    ✅ Ajout de la création de compte #5

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,10 +3,16 @@
 namespace App\Controller;
 
 use LogicException;
+use App\Entity\User;
 use App\Form\LoginType;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use App\Security\LoginFormAuthenticator;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -16,9 +22,7 @@ class SecurityController extends AbstractController {
         Request $request,
         AuthenticationUtils $authenticationUtils
     ): Response {
-        if ($this->getUser()) {
-            $this->redirectToRoute('homepage');
-        }
+        $this->userIsLoggedIn();
 
         $form = $this->createForm(LoginType::class);
         $form->handleRequest($request);
@@ -30,8 +34,40 @@ class SecurityController extends AbstractController {
         ]);
     }
 
+    #[Route('/register', name: 'user_create', methods: ['GET', 'POST'])]
+    public function register(
+        Request $request,
+        UserRepository $userRepository,
+        Security $security
+    ): Response {
+        $this->userIsLoggedIn();
+
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $userRepository->add($user, $form->get('plainPassword')->getData());
+
+            $security->login($user, LoginFormAuthenticator::class);
+
+            $this->addFlash('success', 'Votre compte a été créé avec succès. Vous êtes maintenant connecté. Bienvenue !');
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form,
+        ]);
+    }
+
     #[Route(path: '/logout', name: 'logout')]
     public function logout(): void {
         throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    private function userIsLoggedIn(): ?RedirectResponse {
+        return $this->getUser() ? $this->redirectToRoute('home.home') : null;
     }
 }
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationFormType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur',
+                'attr' => ['autofocus' => true],
+                'help' => 'Votre nom d\'utilisateur doit contenir au moins 4 caractères.'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'help' => 'Votre adresse email ne peut pas contenir plus de 180 caractères.'
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire',
+                'attr' => ['class' => 'btn btn-primary btn-lg'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
