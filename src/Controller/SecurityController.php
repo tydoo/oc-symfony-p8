5e5534005632e945c53ea5257f889b9afd4d466a
tboyer <thomas.boyer2@interieur.gouv.fr>
2024-05-04 19:33:59 +0200

    ✅ Ajout du login #4

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use LogicException;
+use App\Form\LoginType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController {
+    #[Route(path: '/login', name: 'login', methods: ['GET', 'POST'])]
+    public function login(
+        Request $request,
+        AuthenticationUtils $authenticationUtils
+    ): Response {
+        if ($this->getUser()) {
+            $this->redirectToRoute('homepage');
+        }
+
+        $form = $this->createForm(LoginType::class);
+        $form->handleRequest($request);
+        $form->get('username')->setData($authenticationUtils->getLastUsername());
+
+        return $this->render('security/login.html.twig', [
+            'loginForm' => $form,
+            'error' => $authenticationUtils->getLastAuthenticationError()
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'logout')]
+    public function logout(): void {
+        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
---
+++ src/Form/LoginType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class LoginType extends AbstractType {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur',
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom d\'utilisateur',
+                ],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'attr' => [
+                    'placeholder' => 'Entrez votre mot de passe',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Se connecter',
+                'attr' => ['class' => 'btn btn-primary btn-lg'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void {
+        $resolver->setDefaults([
+            'csrf_field_name' => '_csrf_token',
+            'csrf_token_id' => 'authenticate',
+        ]);
+    }
+
+    public function getBlockPrefix() {
+        return '';
+    }
+}
